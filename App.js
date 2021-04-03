31d69617865323d59b970c94523339644f5b8130
Faintzy <gabrielmrts@yahoo.com>
2021-04-03 02:26:28 -0300

    Main file

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Quotes from "./components/Quotes";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Quotes  />
+        <StatusBar style="light" />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#011627',
+    width: '100%',
+    height: '100%',
   },
 });
